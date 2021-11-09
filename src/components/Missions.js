9174ed5dd331fdbc3d505d2e15395081c00816ca
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-09 08:00:20 -0800

    Add table for missions page

---
+++ src/components/Missions.js
@@ -0,0 +1,15 @@
+const Missions = () => (
+  <div className="Table-Container">
+    <table className="Mission-Table">
+      <tr>
+        <th className="Table-Content Mission">Mission</th>
+        <th className="Table-Content Description">Description</th>
+        <th className="Table-Content Status">Status</th>
+        <th className="Table-Content Empty">&nbsp;</th>
+      </tr>
+      <MissionCard />
+    </table>
+  </div>
+);
+
+export default Missions;

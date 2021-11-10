a3a44118792b425ca87e5100ebef82422e8a794b
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-10 16:00:17 +0200

    Add Rockets NavLink in Navabar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/planet.png';
 import './Navbar.css';
 
@@ -7,8 +8,14 @@ const Navbar = () => (
       <img className="Logo-Img" src={logo} alt="planet logo" />
       <span className="Logo-Text">Space Travelers&apos; Hub</span>
     </div>
-    <div className="Navigaion-Links">
-      <ul className="Link-List" />
+    <div className="Navigation-Links">
+      <ul className="Link-List">
+        <li className="Link-Item">
+          <NavLink to="/rockets">
+            Rockets
+          </NavLink>
+        </li>
+      </ul>
     </div>
   </div>
 );

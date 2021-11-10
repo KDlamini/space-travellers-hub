ac10efe25c04ba55495f64335c267bbbcb95de51
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-10 22:59:32 +0200

    Retrive rockets state using useSelector. - dispatch getRockets action using UseEffect. - map Rockets to Rocket component.

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Rocket from './Rocket';
+import getRockets from '../redux/actions/rockets';
 
 function Rockets() {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
   return (
-    <div className="rockets" />
+    <div className="rockets">
+      {
+        rockets.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            id={rocket.id}
+            img_src={rocket.image[0]}
+            name={rocket.name}
+            description={rocket.description}
+            reserved={rocket.reserved}
+          />
+        ))
+      }
+    </div>
   );
 }
 

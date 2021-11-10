409972df3a353dd6cd5ce0dbb44a7b9e99aee82b
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 00:09:21 +0200

    Filter payload data.

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import getRockets from '../redux/actions/rockets';
 
 function Rockets() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
   return (
     <div className="rockets" />
   );
--- src/redux/actions/rockets.js
+++ src/redux/actions/rockets.js
@@ -1,12 +1,25 @@
 import { FETCH_ALL_ROCKETS } from './actionTypes';
 import * as api from '../../api/api';
 
+const filterData = (data) => {
+  const filteredData = data.map((rocket) => {
+    const {
+      id, rocket_name: name, rocket_type: type, flickr_images: image, description,
+    } = rocket;
+    return {
+      id, name, type, image, description,
+    };
+  });
+
+  return filteredData;
+};
+
 // API action creators
 const getRockets = () => async (dispatch) => {
   try {
     const data = await api.fetchRockets();
 
-    dispatch({ type: FETCH_ALL_ROCKETS, payload: data });
+    dispatch({ type: FETCH_ALL_ROCKETS, payload: filterData(data) });
   } catch (error) {
     throw new Error(error.message);
   }

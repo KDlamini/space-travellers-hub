e75b45bc9511f9a29c429a9a28cdebb12ed5a54d
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-10 15:35:26 -0800

    Add event listener to button

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -17,8 +17,18 @@ const MissionCard = (props) => {
     <tr>
       <td className="Table-Content Mission">{name}</td>
       <td className="Table-Content Description Desc-Info">{description}</td>
-      <td className="Table-Content Status Btn"><span className="Member">NOT A MEMBER</span></td>
-      <td className="Table-Content Empty Btn"><button type="submit" className="Join">Join Mission</button></td>
+      <td className="Table-Content Status Btn">
+        <span className="Member">NOT A MEMBER</span>
+      </td>
+      <td className="Table-Content Empty Btn">
+        <button
+          type="submit"
+          className="Join"
+          onClick={joinMissionHandler}
+        >
+          Join Mission
+        </button>
+      </td>
     </tr>
   );
 };

4e7204fa51c42d11d62e26ea7c212838ed1ee437
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-10 23:24:03 +0200

    Update rockets redux reducer.

--- src/redux/reducers/rockets.js
+++ src/redux/reducers/rockets.js
@@ -3,8 +3,7 @@ import { FETCH_ALL_ROCKETS } from '../actions/actionTypes';
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_ROCKETS:
-      // Write code here
-      return state; // TODO: return 'action.payload'
+      return action.payload;
 
     default:
       return state;

85d12ee54ac9faf78b731d9268533c0df6ce9030
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-10 14:53:24 -0800

    Change MissionCard to accept props

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,10 +1,14 @@
-const MissionCard = () => (
-  <tr>
-    <td className="Table-Content Mission">Mission...</td>
-    <td className="Table-Content Description">Description...</td>
-    <td className="Table-Content Status">Button...</td>
-    <td className="Table-Content Empty">Button2...</td>
-  </tr>
-);
+const MissionCard = (props) => {
+  const { name, description } = props;
+
+  return (
+    <tr>
+      <td className="Table-Content Mission">{name}</td>
+      <td className="Table-Content Description Desc-Info">{description}</td>
+      <td className="Table-Content Status Btn"><span className="Member">NOT A MEMBER</span></td>
+      <td className="Table-Content Empty Btn"><button type="submit" className="Join">Join Mission</button></td>
+    </tr>
+  );
+};
 
 export default MissionCard;

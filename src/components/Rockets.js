f63376108e6ca36cce1c969150fca202750d0d49
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 02:09:40 +0200

    Apply changes to rockets render.

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import getRockets from '../redux/actions/rockets';
 
 function Rockets() {
+  const rockets = useSelector((state) => state.rockets);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getRockets());
+    if (!rockets.length) {
+      dispatch(getRockets());
+    }
   }, []);
 
   return (

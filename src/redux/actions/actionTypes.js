f45645b62d446e8fa5255d23f3ba0dc6f6b0c9ed
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 15:10:26 +0200

    Add RESERVE_ROCKET action type and update reducer logic.

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,2 +1,3 @@
 export const FETCH_ALL_MISSIONS = 'spaceHub/api/FETCH_ALL_MISSIONS';
 export const FETCH_ALL_ROCKETS = 'spaceHub/api/FETCH_ALL_ROCKETS';
+export const RESERVE_ROCKET = 'spaceHub/rockets/RESERVE_ROCKET';
--- src/redux/reducers/rockets.js
+++ src/redux/reducers/rockets.js
@@ -1,9 +1,17 @@
-import { FETCH_ALL_ROCKETS } from '../actions/actionTypes';
+import { FETCH_ALL_ROCKETS, RESERVE_ROCKET } from '../actions/actionTypes';
 
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_ROCKETS:
       return action.payload;
+
+    case RESERVE_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id === parseInt(action.payload, 10)) {
+          return { ...rocket, reserved: true };
+        }
+        return { ...rocket };
+      });
 
     default:
       return state;

ca663095cba14099f03ba014a75a01dac2c007e9
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 18:04:59 +0200

    Add my profile content body skeleton.

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import profileRockets from './profileRockets';
 
 function MyProfile() {
   return (
-    <div className="my-profile" />
+    <div className="queryresult">
+      <div className="Box">
+        <h2 className="Cat-Title">My Rockets</h2>
+        <table className="Mission-ProfileTable">
+          <tbody>
+            {profileRockets}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 

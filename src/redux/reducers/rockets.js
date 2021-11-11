3bc356da8fd7e22cfe234d527e33e8f7342d96af
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 16:19:33 +0200

    Add cancel reservation action creator. - Create cancel reservation constant action type.

--- src/redux/reducers/rockets.js
+++ src/redux/reducers/rockets.js
@@ -1,10 +1,17 @@
-import { FETCH_ALL_ROCKETS } from '../actions/actionTypes';
+import { FETCH_ALL_ROCKETS, RESERVE_ROCKET } from '../actions/actionTypes';
 
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_ROCKETS:
-      // Write code here
-      return state; // TODO: return 'action.payload'
+      return action.payload;
+
+    case RESERVE_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id === parseInt(action.payload, 10)) {
+          return { ...rocket, reserved: true };
+        }
+        return { ...rocket };
+      });
 
     default:
       return state;

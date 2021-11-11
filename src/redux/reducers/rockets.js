a50e90fec630b505791e856a7708560f48418c02
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 13:05:34 +0200

    refactor 'handleData' in rockets actions. - Apply handleData functionality in rockets reducer.

--- src/redux/reducers/rockets.js
+++ src/redux/reducers/rockets.js
@@ -1,9 +1,19 @@
+/* eslint-disable no-case-declarations */
 import { FETCH_ALL_ROCKETS } from '../actions/actionTypes';
 
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_ROCKETS:
-      return action.payload;
+      const filteredData = action.payload.map((rocket) => {
+        const {
+          id, rocket_name: name, rocket_type: type, flickr_images: image, description,
+        } = rocket;
+        return {
+          id, name, type, image, description,
+        };
+      });
+
+      return [...state, ...filteredData];
 
     default:
       return state;

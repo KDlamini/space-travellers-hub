0e52dbc4735195829a65c5774986767efd253a93
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-11 14:22:57 -0800

    Add leave mission handler and conditional rendering

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -3,15 +3,18 @@ import { useDispatch } from 'react-redux';
 import { joinMission } from '../redux/actions/missions';
 
 const MissionCard = (props) => {
-  const { name, description, missionid } = props;
+  const {
+    name,
+    description,
+    missionid,
+    reserved,
+  } = props;
+
   const dispatch = useDispatch();
 
-  const joinMissionHandler = (e) => {
-    const button = e.target;
-    button.innerText = 'Leave Mission';
-    button.classList.add('Joined');
-    dispatch(joinMission(missionid));
-  };
+  const joinMissionHandler = () => { dispatch(joinMission(missionid)); };
+
+  const leaveMissionHandler = () => { dispatch(leaveMission(missionid)); };
 
   return (
     <tr>
@@ -21,13 +24,24 @@ const MissionCard = (props) => {
         <span className="Member">NOT A MEMBER</span>
       </td>
       <td className="Table-Content Empty Btn">
-        <button
-          type="submit"
-          className="Join"
-          onClick={joinMissionHandler}
-        >
-          Join Mission
-        </button>
+        {reserved && (
+          <button
+            type="submit"
+            className="Join Joined"
+            onClick={leaveMissionHandler}
+          >
+            Leave Mission
+          </button>
+        )}
+        {!reserved && (
+          <button
+            type="submit"
+            className="Join"
+            onClick="{joinMissionHandler}"
+          >
+           Join Mission
+          </button>
+        )}
       </td>
     </tr>
   );
@@ -37,12 +51,14 @@ MissionCard.defaultProps = {
   name: '',
   description: '',
   missionid: '',
+  reserved: false,
 };
 
 MissionCard.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string,
   missionid: PropTypes.string,
+  reserved: PropTypes.bool,
 };
 
 export default MissionCard;

f497890e029723bc3fb4029cedcd7d2f92edc402
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-11 18:22:31 +0200

    Update MyProfile component.

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import profileRockets from './profileRockets';
+import './MyProfile.css';
 
 function MyProfile() {
+  const rockets = useSelector((state) => state.rockets);
+
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+
   return (
     <div className="queryresult">
       <div className="Box">
         <h2 className="Cat-Title">My Rockets</h2>
         <table className="Mission-ProfileTable">
           <tbody>
-            {profileRockets}
+            {
+              reservedRockets.map((rockets) => (
+                <profileRockets
+                  key={rockets.id}
+                  content={rockets.name}
+                />
+              ))
+            }
           </tbody>
         </table>
       </div>
